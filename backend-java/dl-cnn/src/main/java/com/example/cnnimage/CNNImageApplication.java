deb70b3ea482eef52e0d6ecd28342af873588006
sakshivirhajobsearch <164556079+sakshivirhajobsearch@users.noreply.github.com>
2025-09-05 14:24:17 +0530

    Added

--- backend-java/dl-cnn/src/main/java/com/example/cnnimage/CNNImageApplication.java
+++ backend-java/dl-cnn/src/main/java/com/example/cnnimage/CNNImageApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CnnImageApp {
+public class CNNImageApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(CnnImageApp.class, args);
+		SpringApplication.run(CNNImageApplication.class, args);
 	}
 }
